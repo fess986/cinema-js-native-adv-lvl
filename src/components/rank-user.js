4fdc7acbbd144412b7bc1dbf57a733ea9aa32086
Fess986 <fess986@gmail.com>
2022-08-03 22:23:45 +0300

    adding webpack, files .editorconfig, .eslintignore, .eslintrc.yml, .gitignore sepparating project on components

---
+++ src/components/rank-user.js
@@ -0,0 +1,9 @@
+// создаем шаблон элемента "Звание пользователя"
+export const createRankUser = () => {
+  return (
+    `<section class="header__profile profile">
+    <p class="profile__rating">Movie Buff</p>
+    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+  </section>`
+  );
+};
---
+++ src/components/show-more-button.js
@@ -0,0 +1,5 @@
+export const createShowMoreButton = () => {
+  return (
+    `<button class="films-list__show-more">Show more</button>`
+  );
+};
